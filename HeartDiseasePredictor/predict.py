b1d01a217eb1ce315a6e0bad11d63079326fd925
P-Odiix <mahingarodin@gmail.com>
2025-02-28 18:16:31 +0200

    "updated"

--- HeartDiseasePredictor/predict.py
+++ HeartDiseasePredictor/predict.py
@@ -4,12 +4,12 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 #loading the saved model 
-model_path = "heart_disease_model.pkl"
+model_path = r"C:\Users\user\OneDrive\Desktop\DP\HeartDiseasePredictor\heart_disease_model.pkl"
 with open(model_path, 'rb') as file:
     loaded_model = pickle.load(file)
 
 #load the dataset
-dataset_path = "dataset.csv"
+dataset_path = r"C:\Users\user\OneDrive\Desktop\DP\HeartDiseasePredictor\dataset.csv"
 dataset = pd.read_csv(dataset_path)
 
 #preprocess the data
@@ -25,7 +25,7 @@ dataset[columns_to_scale] = scaler.fit_transform(dataset[columns_to_scale])
 # Example: Select a row from the dataset as new input data (replace with actual row index or data)
 # Let's assume we are predicting for the first row in the dataset
 
-new_sample = dataset.iloc[0].drop('target').values.reshaped(1, -1)  # Drop 'target' column and reshape for prediction
+new_sample = dataset.iloc[0].drop('target').values.reshape(1, -1)  # Drop 'target' column and reshape for prediction
 
 #make predictions 
 prediction = loaded_model.predict(new_sample)
